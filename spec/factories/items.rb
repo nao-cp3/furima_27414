1bd98752e01e982e73f1a3260e79f3eba2fea045
nao-cp3 <pg.cp3.lj23.clippers@gmail.com>
2020-08-03 11:56:01 +0900

    商品出品機能

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
   factory :item do
-    #image {File.open("#{Rails.root}/public/images/test_image.jpg")}
     name {"item"}
     text {"aaa"}
     prices {1000}
+    category_id {2}
+    condition_id {2}
+    postage_id {2}
+    prefecture_id {2}
+    several_days_id {2}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   describe '#create' do
+
+    before do 
+      item = Item.new(name: "item", text: "aaa", prices: "1000", category_id: "2", condition_id: "2", postage_id: "2", prefecture_id: "2", several_days_id: "2")
+      item.save
+    end
     
     it "is invalid without name" do
       item = build(:item, name: nil)
@@ -27,6 +32,34 @@ RSpec.describe Item, type: :model do
       expect(item.errors[:image]).to include("can't be blank")
     end
 
+    it "is invalid without category_id" do
+      item = build(:item, category_id: nil)
+      item.valid?
+      expect(item.errors[:category_id]).to include("is not a number")
+    end
 
+    it "is invalid without condition_id" do
+      item = build(:item, condition_id: nil)
+      item.valid?
+      expect(item.errors[:condition_id]).to include("is not a number")
+    end
+
+    it "is invalid without postage_id" do
+      item = build(:item, postage_id: nil)
+      item.valid?
+      expect(item.errors[:postage_id]).to include("is not a number")
+    end
+
+    it "is invalid without prefecture_id" do
+      item = build(:item, prefecture_id: nil)
+      item.valid?
+      expect(item.errors[:prefecture_id]).to include("is not a number")
+    end
+
+    it "is invalid without several_days_id" do
+      item = build(:item, several_days_id: nil)
+      item.valid?
+      expect(item.errors[:several_days_id]).to include("is not a number")
+    end
   end
 end
