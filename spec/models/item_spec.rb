e8953cb85fa9345dc71e8a617af25bb446e169a4
nao-cp3 <pg.cp3.lj23.clippers@gmail.com>
2020-08-03 12:38:21 +0900

    商品出品機能

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,62 +4,62 @@ RSpec.describe Item, type: :model do
   describe '#create' do
 
     before do 
-      item = Item.new(name: "item", text: "aaa", prices: "1000", category_id: "2", condition_id: "2", postage_id: "2", prefecture_id: "2", several_days_id: "2")
-      item.save
+      @item = FactoryBot.build(:item)
+      @item.save
     end
     
     it "is invalid without name" do
-      item = build(:item, name: nil)
-      item.valid?
-      expect(item.errors[:name]).to include("can't be blank")
+      @item.name = nil
+      @item.valid?
+      expect(@item.errors[:name]).to include("can't be blank")
     end
 
     it "is invalid without text" do
-      item = build(:item, text: nil)
-      item.valid?
-      expect(item.errors[:text]).to include("can't be blank")
+      @item.text =  nil
+      @item.valid?
+      expect(@item.errors[:text]).to include("can't be blank")
     end
 
     it "is invalid without prices" do
-      item = build(:item, prices: nil)
-      item.valid?
-      expect(item.errors[:prices]).to include("can't be blank")
+      @item.prices = nil
+      @item.valid?
+      expect(@item.errors[:prices]).to include("can't be blank")
     end
 
     it "is invalid without image" do
-      item = build(:item, image: nil)
-      item.valid?
-      expect(item.errors[:image]).to include("can't be blank")
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors[:image]).to include("can't be blank")
     end
 
     it "is invalid without category_id" do
-      item = build(:item, category_id: nil)
-      item.valid?
-      expect(item.errors[:category_id]).to include("is not a number")
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors[:category_id]).to include("must be other than 1")
     end
 
     it "is invalid without condition_id" do
-      item = build(:item, condition_id: nil)
-      item.valid?
-      expect(item.errors[:condition_id]).to include("is not a number")
+      @item.condition_id = 1
+      @item.valid?
+      expect(@item.errors[:condition_id]).to include("must be other than 1")
     end
 
     it "is invalid without postage_id" do
-      item = build(:item, postage_id: nil)
-      item.valid?
-      expect(item.errors[:postage_id]).to include("is not a number")
+      @item.postage_id = 1
+      @item.valid?
+      expect(@item.errors[:postage_id]).to include("must be other than 1")
     end
 
     it "is invalid without prefecture_id" do
-      item = build(:item, prefecture_id: nil)
-      item.valid?
-      expect(item.errors[:prefecture_id]).to include("is not a number")
+      @item.prefecture_id = 1
+      @item.valid?
+      expect(@item.errors[:prefecture_id]).to include("must be other than 1")
     end
 
     it "is invalid without several_days_id" do
-      item = build(:item, several_days_id: nil)
-      item.valid?
-      expect(item.errors[:several_days_id]).to include("is not a number")
+      @item.several_days_id = 1
+      @item.valid?
+      expect(@item.errors[:several_days_id]).to include("must be other than 1")
     end
   end
 end
