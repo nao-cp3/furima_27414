e38ea87c5c3e374343099d710cf2c6a98caf5990
nao-cp3 <pg.cp3.lj23.clippers@gmail.com>
2020-08-04 18:01:49 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :update, :show]
+  before_action :set_item, only: [:destroy, :edit, :update, :show]
 
   def index
     @items = Item.order("created_at DESC")
@@ -15,6 +15,12 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
     end
   end
 
