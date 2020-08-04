e823aea4efb2311efd7c71e130f7b5d147bcb691
nao-cp3 <pg.cp3.lj23.clippers@gmail.com>
2020-08-04 17:17:10 +0900

    商品編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :update, :show]
 
   def index
     @items = Item.order("created_at DESC")
@@ -22,7 +22,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
